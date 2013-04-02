0a43f6771bf9bc122ada621696896088da4016d5
mabarroso <mabarroso@mabarroso.com>
2013-04-02 12:28:48 +0200

    BDD test configuration

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,17 @@
+<?php
+
+use Behat\Behat\Context\ClosuredContextInterface,
+    Behat\Behat\Context\TranslatedContextInterface,
+    Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+use Behat\MinkExtension\Context\MinkContext;
+
+/**
+ * Features context.
+ */
+class FeatureContext extends MinkContext
+{
+}
